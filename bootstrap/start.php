db29d5ecb7a8b02ec10f4bcc4430105bfbe397ba
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2014-08-04 15:17:23 -0700

    add environment detect

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('homestead'),
+	'local'         => array('homestead'),
+    'production'    => array('weather')
 
 ));
 
