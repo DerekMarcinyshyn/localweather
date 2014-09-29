b960d84e3eeb0e826d2c0a4199d4897e51c8cef6
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2014-09-28 20:12:19 -0700

    change times for video

--- app/commands/CleanImageCommand.php
+++ app/commands/CleanImageCommand.php
@@ -47,7 +47,7 @@ class CleanImageCommand extends ScheduledCommand {
 	 */
 	public function schedule(Schedulable $scheduler)
 	{
-		return $scheduler->setSchedule('*/5', [4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21], '*', '*', '*');
+		return $scheduler->setSchedule('*/5', [5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], '*', '*', '*');
 	}
 
 	/**
