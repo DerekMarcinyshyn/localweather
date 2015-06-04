b2cb00ade5a55323fafc0d2f67d5b72c85d866ff
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2015-06-03 23:57:02 -0700

    add more hours to timelapse

--- app/commands/CleanImageCommand.php
+++ app/commands/CleanImageCommand.php
@@ -45,7 +45,7 @@ class CleanImageCommand extends ScheduledCommand {
 	 */
 	public function schedule(Schedulable $scheduler)
 	{
-		return $scheduler->setSchedule('*/5', [4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], '*', '*', '*');
+		return $scheduler->setSchedule('*/5', [4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21], '*', '*', '*');
 	}
 
 	/**
